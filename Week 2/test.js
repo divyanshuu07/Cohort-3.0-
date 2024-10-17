6dc657dfb6ea96e940dd3e57099ca7850a509571
divyanshuu07 <divyanshu9131@outlook.com>
2024-10-17 22:59:51 +0530

    Updating test.js

--- Week 2/test.js
+++ Week 2/test.js
@@ -1,6 +1,6 @@
 const fs=require("fs");
 
-const contents=fs.readFile("a.txt","utf8"); //asynchronously
+const contents=fs.readFile("a.txt","utf8"); //asynchronously(the program is on hold)
 console.log(contents);
 const contents2=fs.readFileSync("b.txt","utf-8"); //synchronously
 console.log(contents2);
