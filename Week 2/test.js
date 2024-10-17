bdc9ad1310d2f45761a31ae2117b2f6f48ed0a6d
divyanshuu07 <divyanshu9131@outlook.com>
2024-10-17 23:18:11 +0530

    Updating test.js

--- Week 2/test.js
+++ Week 2/test.js
@@ -1,6 +1,27 @@
-const fs=require("fs");
+//Difference between Asynchronous and Synchronous
 
-const contents=fs.readFile("a.txt","utf8"); //asynchronously(the program is on hold)
-console.log(contents);
-const contents2=fs.readFileSync("b.txt","utf-8"); //synchronously
-console.log(contents2);
+// const fs=require("fs");
+// const contents=fs.readFile("a.txt","utf8"); //asynchronously(the program is on hold)
+// console.log(contents);
+// const contents2=fs.readFileSync("b.txt","utf-8"); //synchronously
+// console.log(contents2);
+
+
+//Functional arguments
+
+function sum(a,b){
+    return a+b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+function subtract(a,b){
+    return a-b;
+}
+
+function doOperation(a,b,op){
+    return op(a,b);
+}
+
+console.log(doOperation(2,3,subtract));
