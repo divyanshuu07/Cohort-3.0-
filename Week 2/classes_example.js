4450082b931005f4f8ea267db2fd06a48ef35c58
divyanshuu07 <divyanshu9131@outlook.com>
2024-12-06 14:19:26 +0530

    Adding examples of Classes

---
+++ Week 2/classes_example.js
@@ -0,0 +1,14 @@
+//playing with the Date Class.
+const date=new Date();
+console.log(date);
+
+console.log(date.getTime());
+//it returns the number of ms that have elapsed since 1 Jan, 1970, 00:00:00 UTC(also known as UNIX Eppch).
+//as date is a class. it comes with different functions.
+
+
+//Map class
+const map=new Map();
+map.set('name','Divyanshu');
+map.set('age',21);
+console.log(map.get('name'));
