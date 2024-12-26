3bb57fe1cdf46e6e259b3d45c85f47caa1e03043
divyanshuu07 <divyanshu9131@outlook.com>
2024-12-26 18:39:54 +0530

    Finishing basic express

--- Week 4/Express/index.js
+++ Week 4/Express/index.js
@@ -6,4 +6,18 @@ const express=require("express");
 
 const app=express();
 
+function sum(n){
+    return (n*(n+1))/2;
+}
+
+app.get("/",function(req,res){
+    const n=Number(req.query.n);
+    if(isNaN(n)){
+        res.send("sahi input de vro");
+        return;
+    }
+    const ans=sum(n);
+    res.send("Output of first n numbers is: "+ans);
+})
+
 app.listen(3000);
